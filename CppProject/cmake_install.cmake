3b5fcce5e54cb3817dd0bcb884e9dd1deccd9be0
Rajasekhar Chintalapudi <rajasekhar.chintalapudi@volvocars.com>
2022-11-06 12:19:11 +0100

    Changed Project Structure

--- CppProject/cmake_install.cmake
+++ CppProject/cmake_install.cmake
@@ -42,30 +42,6 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xbinx" OR NOT CMAKE_INSTALL_COMPONENT)
-  if(EXISTS "$ENV{DESTDIR}/home/rchintal/Raj_Workspace/mycppproject/CppProject/bin/cpp_project" AND
-     NOT IS_SYMLINK "$ENV{DESTDIR}/home/rchintal/Raj_Workspace/mycppproject/CppProject/bin/cpp_project")
-    file(RPATH_CHECK
-         FILE "$ENV{DESTDIR}/home/rchintal/Raj_Workspace/mycppproject/CppProject/bin/cpp_project"
-         RPATH "")
-  endif()
-  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
-   "/home/rchintal/Raj_Workspace/mycppproject/CppProject/bin/cpp_project")
-  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
-    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
-  endif()
-  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
-    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
-  endif()
-  file(INSTALL DESTINATION "/home/rchintal/Raj_Workspace/mycppproject/CppProject/bin" TYPE EXECUTABLE FILES "/home/rchintal/Raj_Workspace/mycppproject/CppProject/cpp_project")
-  if(EXISTS "$ENV{DESTDIR}/home/rchintal/Raj_Workspace/mycppproject/CppProject/bin/cpp_project" AND
-     NOT IS_SYMLINK "$ENV{DESTDIR}/home/rchintal/Raj_Workspace/mycppproject/CppProject/bin/cpp_project")
-    if(CMAKE_INSTALL_DO_STRIP)
-      execute_process(COMMAND "/usr/bin/strip" "$ENV{DESTDIR}/home/rchintal/Raj_Workspace/mycppproject/CppProject/bin/cpp_project")
-    endif()
-  endif()
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
