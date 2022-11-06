1e2d8af9087033231ce0983def6214b4f3e2ff34
Rajasekhar Chintalapudi <rajasekhar.chintalapudi@volvocars.com>
2022-11-06 12:19:11 +0100

    Changed Project Structure

--- CppProject/CMakeFiles/cpp_project.dir/cmake_clean.cmake
+++ CppProject/CMakeFiles/cpp_project.dir/cmake_clean.cmake
@@ -5,13 +5,8 @@ file(REMOVE_RECURSE
   "CMakeFiles/cpp_project.dir/src/database_connect.cc.o.d"
   "CMakeFiles/cpp_project.dir/src/operator_overloading.cc.o"
   "CMakeFiles/cpp_project.dir/src/operator_overloading.cc.o.d"
-<<<<<<< HEAD
-  "cpp_project"
-  "cpp_project.pdb"
-=======
   "bin/cpp_project"
   "bin/cpp_project.pdb"
->>>>>>> a1994b6 (Changed Project Structure)
 )
 
 # Per-language clean rules from dependency scanning.
